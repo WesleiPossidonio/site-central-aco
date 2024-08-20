34e95f2227bcf87fa37fe07311141d8317a4ce57
Weslei Possidonio <Weslei.possidonio@gmail.com>
2024-08-20 19:00:48 -0300

    update banner slider

--- src/pages/Home/Components/Hero/index.tsx
+++ src/pages/Home/Components/Hero/index.tsx
@@ -19,7 +19,7 @@ export const BannerMain = () => {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3500,
           disableOnInteraction: false,
         }}
         pagination={{
